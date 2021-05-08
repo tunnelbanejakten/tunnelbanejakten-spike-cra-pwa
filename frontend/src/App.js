7917c4fda48cd6c93d8468a5b0a76a1c23d16fd8
Mikael Svensson <mikael.svensson@klarna.com>
2021-05-08 16:06:36 +0200

    Status page to verify that front-end can make API requests #4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const apiHost = process.env.REACT_APP_API_HOST
+    const [pingResponse, setPingResponse] = useState('')
+
+    useEffect(() => {
+        const pingApi = async () => {
+            try {
+                const resp = await fetch(`${apiHost}/api/ping`)
+                const payload = await resp.json()
+                setPingResponse(`API is online. Ping message: ${payload.message}.`);
+            } catch (e) {
+                setPingResponse('API is not available');
+            }
+        }
+        pingApi()
+    }, [setPingResponse, apiHost])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    {pingResponse || 'API status is unknown'}
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
